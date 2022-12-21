fa492ea075c5337c3b8e804138d3c93053be6ea0
Fabricio Travassos <fabricio.travassos@uem.com.br>
2022-12-20 22:30:06 -0300

    Questão 2

--- Atividades/Program.cs
+++ Atividades/Program.cs
@@ -4,54 +4,42 @@
     {
         static void Main(string[] args)
         {
-            // Perguntando ao usuário o número de cidades
-            uint numCidades;        // Armazena o número de cidades
-            bool numCidadesValido;  // Verifica se a cidade informada é válida
+            // Armazenando caminho para o desktop
+            string  diretorioDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
+            string  localMatriz  = diretorioDesktop + @"\matriz.txt";   // Endereço da matriz
+            string  localCaminho = diretorioDesktop + @"\caminho.txt";  // Endereço do caminho
 
-            do
+            // Verificando se os arquivos necessários estão no desktop
+            bool arquivoExiste = File.Exists(localMatriz) && File.Exists(localCaminho);  
+
+            if (arquivoExiste)
             {
-                Console.Write("Digite o número de cidades: ");
-                numCidadesValido = uint.TryParse(Console.ReadLine(), out numCidades);
-                if (!numCidadesValido || numCidades == 1) Console.WriteLine("Valor inválido.");
-            } while (!numCidadesValido || numCidades == 1);
+                // Lendo o arquvio de distâncias, subistuindo as quebras de linha por ',' para usar o split para separar elas
+                uint[] vetorDistancias = File.ReadAllText(localMatriz).Replace(Environment.NewLine,",").Split(",").Select(uint.Parse).ToArray();
+                // Como a matriz sempre tem que ser quadrada, fazendo o cálculo da raiz quadrada para saber o número de cidades
+                var  numCidades = (uint)Math.Sqrt(vetorDistancias.Length);
 
-            // Criando o array bidimensional que vai armazenar as distâncias entre as cidades
-            uint[,] tabelaDistancias = new uint[numCidades, numCidades];    // Array iniciado em 0, não sendo necessário atribuir valores a sua diagonal principal
-            bool distanciaValida;                                           // Variável utilizada para verificar se o valor digitado é um valor válido 
+                uint[,] tabelaDistancias = new uint[numCidades, numCidades];
 
-            // Preenchendo a tabela de distâncias no loop
-            for (int i = 0; i < numCidades; i++)
-            {
-                for (int j = i + 1; j < numCidades; j++)
-                {
-                    Console.Write($"Digite as distâncias entre as cidades {i + 1} e {j + 1}: ");
-                    distanciaValida = uint.TryParse(Console.ReadLine(), out tabelaDistancias[i, j]);
+                // Percorrendo o vetor de distâncias para preencher a tabela
+                for (int i = 0; i < vetorDistancias.Length; i++)
+                    tabelaDistancias[i / numCidades, i % numCidades] = vetorDistancias[i];
 
-                    if (!distanciaValida) // Verificando se o valor digitado não é válido
-                    {
-                        Console.WriteLine("O valor digitado é inválido, tente novamente.");
-                        j--;    // Decrementando j para permanecer no loop caso o valor digitado seja inválido
-                    }
-                    else        // Caso o valor seja válido, valor simetrico da matriz
-                    {
-                        tabelaDistancias[j, i] = tabelaDistancias[i, j];
-                    }
+                // Lendo a trajetória
+                uint[] cidadesPercorridas = File.ReadAllText(localCaminho).Split(",").Select(uint.Parse).ToArray();
+                uint distanciaPercorrida = 0;
 
-                }
+                // Calculando a distância
+                for (int i = 1; i < cidadesPercorridas.Length; i++)
+                    distanciaPercorrida += tabelaDistancias[cidadesPercorridas[i - 1] - 1, cidadesPercorridas[i] - 1];
+
+                Console.WriteLine($"A distância total percorrida foi: {distanciaPercorrida} km");
             }
-
-            // Perguntando o percurso ao usuário
-            Console.WriteLine($"{Environment.NewLine}Digite o percurso: ");
-            int[] cidadesPercorridas = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
-
-            // Fazendo o cálculo da distância percorrida:
-            uint distanciaPercorrida = 0;
-            for (int i = 1; i < cidadesPercorridas.Length; i++)
+            else // Caso o arquivo não exista, exibir mensagem de erro
             {
-                distanciaPercorrida += tabelaDistancias[cidadesPercorridas[i - 1] - 1, cidadesPercorridas[i] - 1];
+                Console.WriteLine("O arquivo não existe.");
             }
-
-            Console.WriteLine($"A distância total percorrida foi: {distanciaPercorrida} km");
         }
     }
 }
