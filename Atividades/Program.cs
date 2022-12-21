a7336234f711d13edb5242711915730339b43a67
Fabricio Travassos <fabricio.travassos@uem.com.br>
2022-12-21 00:31:02 -0300

    Questão 3

--- Atividades/Program.cs
+++ Atividades/Program.cs
@@ -1,40 +1,61 @@
-﻿namespace Atividades
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Atividades
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+
             // Armazenando caminho para o desktop
             string  diretorioDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             
             string  localMatriz  = diretorioDesktop + @"\matriz.txt";   // Endereço da matriz
             string  localCaminho = diretorioDesktop + @"\caminho.txt";  // Endereço do caminho
 
-            // Verificando se os arquivos necessários estão no desktop
-            bool arquivoExiste = File.Exists(localMatriz) && File.Exists(localCaminho);  
 
-            if (arquivoExiste)
+            using var reader = new StreamReader(localMatriz);
+            using var csv = new CsvParser(reader, config);
+
+            if (csv.Read())
             {
-                // Lendo o arquvio de distâncias, subistuindo as quebras de linha por ',' para usar o split para separar elas
-                uint[] vetorDistancias = File.ReadAllText(localMatriz).Replace(Environment.NewLine,",").Split(",").Select(uint.Parse).ToArray();
-                // Como a matriz sempre tem que ser quadrada, fazendo o cálculo da raiz quadrada para saber o número de cidades
-                var  numCidades = (uint)Math.Sqrt(vetorDistancias.Length);
-
+                var  numCidades = csv.Record.Length;
                 uint[,] tabelaDistancias = new uint[numCidades, numCidades];
 
                 // Percorrendo o vetor de distâncias para preencher a tabela
-                for (int i = 0; i < vetorDistancias.Length; i++)
-                    tabelaDistancias[i / numCidades, i % numCidades] = vetorDistancias[i];
+                for(int i = 0; i < numCidades; i++)
+                {
+                    var posRelativa = csv.Record;   // vetor com as posições de cada cidade em relação a cidade i
+                    for(int j = 0; j < numCidades; j++)
+                    {
+                        tabelaDistancias[i, j] = uint.Parse(posRelativa[j]);
+                    }
+                    csv.Read();
+                }
 
                 // Lendo a trajetória
-                uint[] cidadesPercorridas = File.ReadAllText(localCaminho).Split(",").Select(uint.Parse).ToArray();
-                uint distanciaPercorrida = 0;
+                using var readerCaminho = new StreamReader(localCaminho);
+                using var csvCaminho = new CsvParser(readerCaminho, config);
 
-                // Calculando a distância
-                for (int i = 1; i < cidadesPercorridas.Length; i++)
-                    distanciaPercorrida += tabelaDistancias[cidadesPercorridas[i - 1] - 1, cidadesPercorridas[i] - 1];
+                if (csvCaminho.Read())
+                {
+                    uint[] cidadesPercorridas = csvCaminho.Record.Select(uint.Parse).ToArray();
 
-                Console.WriteLine($"A distância total percorrida foi: {distanciaPercorrida} km");
+                    uint distanciaPercorrida = 0;
+
+                    // Calculando a distância
+                    for (int i = 1; i < cidadesPercorridas.Length; i++)
+                        distanciaPercorrida += tabelaDistancias[cidadesPercorridas[i - 1] - 1, cidadesPercorridas[i] - 1];
+
+                    Console.WriteLine($"A distância total percorrida foi: {distanciaPercorrida} km");
+                }
             }
             else // Caso o arquivo não exista, exibir mensagem de erro
             {
